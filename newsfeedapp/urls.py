680e77cd44941759afc738718c81a83c1ff8fc38
Jahurul Islam <jahurul25@gmail.com>
2022-06-20 23:21:45 +0600

    User login page html and function created

--- newsfeedapp/urls.py
+++ newsfeedapp/urls.py
@@ -3,4 +3,5 @@ from newsfeedapp import views
 
 urlpatterns = [ 
     path('', views.login, name="login"),
+    path('home/', views.home, name="home"),
 ]
--- newsfeedapp/views.py
+++ newsfeedapp/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def login(request):
+    try:
+        if request.method == "GET":
+
+            return render(request, 'login.html')
+        elif request.method == "POST":
+            user_name = request.POST.get('user_name')
+            user_pass = request.POST.get('user_pass')
+
+            return render(request, 'login.html')
+    except:
+        return render(request, 'login.html')
+
+def home(request):
+
     return render(request, 'login.html')
